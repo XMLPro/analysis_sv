1136727d23e1345544f898d243a7395bf277540a
Inazuma110 <c011703534@edu.teu.ac.jp>
2019-10-28 01:55:15 +0900

    ダウンロード時に進化後やクラスの情報も保持するようにした

--- collect.py
+++ collect.py
@@ -2,6 +2,8 @@ import requests
 import bs4
 from tqdm import tqdm
 import time
+import pandas as pd
+import re
 
 
 def get_image():
@@ -23,9 +25,69 @@ def get_image():
         count += 1
         time.sleep(1)
 
-
     print('finished collecting img')
 
-if __name__ == '__main__':
-    get_image()
+def get_card_info(path='./img/'):
+    count = 0
+    url = 'https://shadowverse-portal.com/cards?m=index&lang=ja&m=index&card_offset='
+    info = pd.DataFrame(index=['path', 'type', 'class', 'rarity', 'cv', 'pack', 'skill'])
 
+    while True:
+        source = requests.get(url + str(count * 12)).text
+
+        soup = bs4.BeautifulSoup(source, 'html.parser')
+        cards = soup.select(".el-card-visual-content")
+
+        for card in tqdm(cards):
+            card_url = f'https://shadowverse-portal.com/{card["href"]}?lang=ja'
+            card_html = requests.get(card_url).text
+            card_soup = bs4.BeautifulSoup(card_html, 'html.parser')
+
+            name = card_soup.select_one('.card-main-title').text
+            name = name.replace('\n', '')
+            name = name.replace('\r', '')
+            info[name] = 'None'
+
+            card_info_soup = card_soup.select('.card-info-content span')
+            for num, card_info in enumerate(card_info_soup):
+                card_info = card_info.text
+                card_info = card_info.replace('\n', '')
+                card_info = card_info.replace('\r', '')
+
+                if num == 1:
+                    info[name]['type'] = card_info
+                if num == 3:
+                    info[name]['class'] = card_info
+                if num == 5:
+                    info[name]['rarity'] = card_info
+                if num == 12:
+                    info[name]['cv'] = card_info
+                if num == 14:
+                    info[name]['pack'] = card_info
+
+            info[name]['path'] = path + 'normal/' + name + '.png'
+            img_list = card_soup.select('.card-main-image')
+            for i, img in enumerate(img_list):
+                img = img.select('img')
+                for img_url in img[::2]:
+                    img_url = img_url['src']
+                    if i == 1:
+                        info[f'evo_{name}'] = info[name]
+                        name = f'evo_{name}'
+                        info[name]['path'] = path + 'evo/' + name + '.png'
+                    with open(info[name]['path'], 'wb') as f:
+                        f.write(requests.get(img_url).content)
+
+                    time.sleep(1)
+
+        count += 1
+
+    print('finished!!')
+    return info
+
+
+
+if __name__ == '__main__':
+    info = get_card_info()
+    info.to_pickle('./cards_info.pickle')
+    print(info)
